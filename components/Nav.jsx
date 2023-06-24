12c8caf648d00391d86410fae3d7168ddf974237
dipesh ojha <dipeshojha111@gmail.com>
2023-06-24 16:40:46 +0545

    init repo

---
+++ components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="bg-slate-400 py-8 px-4">
+      <div className="flex justify-between w-4/5 mx-auto ">
+        <div className="text-4xl text-gray-100">logo here</div>
+        <ul className="flex gap-4 text-2xl text-gray-100">
+          <Link href={"#"}>
+            <li className="hover:scale-110 transition-all duration-200 ease-in-out">
+              Home
+            </li>
+          </Link>
+          <Link href={"/products"}>
+            <li className="hover:scale-110 transition-all duration-200 ease-in-out">
+              Products
+            </li>
+          </Link>
+          <Link href={"#"}>
+            <li className="hover:scale-110 transition-all duration-200 ease-in-out">
+              Reports
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+}
